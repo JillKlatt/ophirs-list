09fd80a424243058b6c1b3c80b6301c9b39de492
JillKlatt <klatt.jill@gmail.com>
2021-09-02 16:54:17 -0400

    Whole lotta stuff, Add random functionality and completed tasks on single page

--- src/App.js
+++ src/App.js
@@ -3,11 +3,17 @@ import Header from './components/Header'
 import data from './components/data.json'
 import { useState } from 'react'
 import List from './components/List/List'
-import Form from './components/Form'
+import SingleItem from './components/Task/SingleItem'
 
 function App() {
 
 const [toDoList, setToDoList] = useState(data)
+//let first = toDoList.find(task => !task.complete)
+let completed = toDoList.filter(task => !task.complete)
+
+const [showCompleted, setShowCompleted] = useState(true)
+const [stressed, setStressed] = useState(false)
+const [random, setRandom] = useState(completed[Math.floor(Math.random(0, completed.length) * 10)])
 
 const addTask = (input) => {
   let updatedList = [...toDoList]
@@ -19,22 +25,50 @@ const handleToggle = (id) => {
   let mapped = toDoList.map(task => {
     return task.id == id ? { ...task, complete: !task.complete } : { ...task};
     });
+    console.log(mapped)
 
   setToDoList(mapped);
-  // Using Class Components: this.setState({toDoList: mapped})
 }
 
-const handleFilter = () => {
+const completeNewRandom = (id) => {
+  handleToggle(id)
+  setRandom(completed[Math.floor(Math.random(0, completed.length) * 10)])
+  handleStressed(random)
+}
+
+const handleFilter = () => {  
+  if (showCompleted){
   let filtered = toDoList.filter(task => {
     return !task.complete})
-  setToDoList(filtered)
+  setToDoList(filtered)}
+  else {
+    setToDoList(data)
+  }
 }
+
+const toggleStressed = () => {
+  stressed ? setStressed(false) : setStressed(true)
+}
+
+const changeRandom = () => {
+  setRandom(completed[Math.floor(Math.random(0, completed.length) * 10)])
+}
+
+const handleStressed = (random) => {
+  if (stressed){
+    // let random = completed[Math.floor(Math.random(0, completed.length) * 10)]
+    return <SingleItem todo={random} changeRandom={changeRandom} handleToggle={handleToggle} completeNewRandom={completeNewRandom}/>
+  }else{
+   return <List toDoList={toDoList} handleFilter={handleFilter} handleToggle={handleToggle} addTask={addTask} showCompleted={showCompleted} setShowCompleted={setShowCompleted}/>
+  }
+}
+
 
   return (
     <div className="App">
       <Header />
-      <List toDoList={toDoList} handleFilter={handleFilter} handleToggle={handleToggle} />
-      <Form addTask={addTask}/>
+      {handleStressed(random)}
+      <button onClick={toggleStressed}>{stressed === false ? "THIS IS TOO MUCH" : "Show it all"}</button>
     </div>
   );
 }
--- src/components/List/List.js
+++ src/components/List/List.js
@@ -1,14 +1,20 @@
 import Task from '../Task/Task'
+import Form from '../Form'
 
-const List = ({toDoList, handleToggle, handleFilter}) => {
-
+const List = ({toDoList, handleToggle, handleFilter, addTask, showCompleted, setShowCompleted}) => {
+    
+    const toggleCompleted = () => {
+        showCompleted ? setShowCompleted(false) : setShowCompleted(true)
+        handleFilter()
+    }
 
     return (
         <div>
             {toDoList.map(todo => {
                 return <Task todo={todo} handleToggle={handleToggle} />
             })}
-            <button style={{margin: `20px`}} onClick={handleFilter}>Clear Completed</button>
+            <button style={{margin: `20px`}} onClick={toggleCompleted}>{showCompleted ? "Clear Completed" : "Show Completed"}</button>
+            <Form addTask={addTask} />
         </div>
     );
 }
