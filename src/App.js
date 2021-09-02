6ed738ea666d119cbacbf1a83b3b6718dfb7f30f
JillKlatt <klatt.jill@gmail.com>
2021-09-02 14:37:17 -0400

    Update app.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
 import './App.css';
+import Header from './components/Header'
+import data from './components/data.json'
+import { useState } from 'react'
+import List from './components/List/List'
+import Form from './components/Form'
 
 function App() {
+
+const [toDoList, setToDoList] = useState(data)
+
+const addTask = (input) => {
+  let updatedList = [...toDoList]
+  updatedList = [...updatedList, { id: toDoList.length + 1, task: input, complete: false}]
+  setToDoList(updatedList)
+}
+
+const handleToggle = (id) => {
+  let mapped = toDoList.map(task => {
+    return task.id == id ? { ...task, complete: !task.complete } : { ...task};
+    });
+
+  setToDoList(mapped);
+  // Using Class Components: this.setState({toDoList: mapped})
+}
+
+const handleFilter = () => {
+  let filtered = toDoList.filter(task => {
+    return !task.complete})
+  setToDoList(filtered)
+}
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <List toDoList={toDoList} handleFilter={handleFilter} handleToggle={handleToggle} />
+      <Form addTask={addTask}/>
     </div>
   );
 }
 
 export default App;
+
