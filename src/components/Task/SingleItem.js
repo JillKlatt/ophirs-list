104d05aa21ce1c3d22a8345cbe54cb20ca445770
JillKlatt <klatt.jill@gmail.com>
2021-09-03 21:03:51 -0400

    Move random logic to single item

--- src/components/Task/SingleItem.js
+++ src/components/Task/SingleItem.js
@@ -1,19 +1,35 @@
 
 
-export default function SingleItem({todo, changeRandom, completeNewRandom}) {
-    console.log(todo)
+export default function SingleItem({inCompleted, completeNewRandom}) {
+    let todo
+    
+    const findRandom = () => {
+        let number = Math.floor(Math.random(0, inCompleted.length - 1) * 10)
+        console.log("current number:", number, "inCompleted:", inCompleted)
+        todo = inCompleted[number]
+    }
+
+    findRandom()
 
     const completeTask = () => {
         completeNewRandom(todo.id)
+    }
+
+    if (inCompleted.length === 1){
+        return (
+            <div>
+                <h1>Out of To Dos</h1>
+            </div>
+        )
     }
 
 
     return (
         <div>
             <h2>THIS IS IT. THIS IS THE THING YOU DO</h2>
-            
-            <h3 id={todo.id} key={todo.id + todo.task} name="todo" value={todo.id} >{todo.task}</h3>
-            <button onClick={changeRandom}>Nah, not this</button>    
+            {(inCompleted.length === 0) ? null : 
+            <h3 id={todo.id} key={todo.id + todo.task} name="todo" value={todo.id} >{todo.task}</h3>}
+            <button onClick={findRandom}>Nah, not this</button>    
             <button onClick={completeTask}>COMPLETE</button>      
         </div>
     )
