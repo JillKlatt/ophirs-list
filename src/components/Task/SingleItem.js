a7fc6a226627878189f03f3486578f880ac8f097
JillKlatt <klatt.jill@gmail.com>
2021-09-04 01:29:46 -0400

    Add styling

--- src/components/Task/SingleItem.js
+++ src/components/Task/SingleItem.js
@@ -7,32 +7,36 @@ export default function SingleItem({inCompleted, deleteTask}) {
     const [todo, setTodo] = useState(inCompleted[number])
 
     const findRandom = () => {
+        console.log(todo)
         number = Math.floor(Math.random(0, inCompleted.length - 1) * 10)
         setTodo(inCompleted[number])
     }
 
-
     const completeTask = () => {
-        console.log(todo)
+        if (todo){
         deleteTask(todo.id)
         findRandom()
+        } else {
+            allOut()
+        }
     }
 
-    if (inCompleted.length === 1){
+    const allOut = () =>{
+    if (inCompleted.length === 0){
         return (
             <div>
                 <h1>Out of To Dos</h1>
             </div>
         )
-    }
+    }}
 
     return (
         <div>
             <h2>THIS IS IT. THIS IS THE THING YOU DO</h2>
-            {(inCompleted.length === 0) ? null : 
-            <h3 id={todo.id} key={todo.id + todo.task} name="todo" value={todo.id} >{todo.task}</h3>}
+            {(!todo) ? allOut() : 
+            <h3 className='single-item' id={todo.id} key={todo.id + todo.task} name="todo" value={todo.id} >{todo.task}</h3>}
             <button onClick={findRandom}>Nah, not this</button>    
-            <button onClick={completeTask}>COMPLETE</button>      
+            <button onClick={completeTask}>FUCKING DONE BRO</button>      
         </div>
     )
 }
